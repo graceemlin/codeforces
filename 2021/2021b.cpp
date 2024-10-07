5ecbddef9d6a171c2dbf19333c6e109ad8ba58d2
graceemlin <110770777+graceemlin@users.noreply.github.com>
2024-10-06 19:53:00 -0700

    cf

---
+++ 2021/2021b.cpp
@@ -0,0 +1,80 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(int argc, char* argv[]) {
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  int t;
+  cin >> t;
+
+  while (t--) {
+    long long n;
+    cin >> n;
+    long long x;
+    cin >> x;
+
+    vector<long long> elements(n);
+    for (int i = 0; i < n; ++i) {
+      cin >> elements[i];
+    }
+
+    bool no_ops = x >= n;
+    bool can_start = *min_element(elements.begin(), elements.end()) == 0;
+    
+    if (no_ops && can_start) {
+      long long curr_nat = 0;
+      long long last = -1;
+      
+      sort(elements.begin(), elements.end());      
+      
+      for (long long element: elements) {
+	if (curr_nat == element && last != element) {
+	  last = curr_nat++;
+	  continue;
+	} else if (curr_nat == element && last == element) {
+	  last = element;
+	  continue;
+	} else if (curr_nat != element && last != element) {
+	  break;
+	}
+      }
+      
+      cout << curr_nat << endl;
+      continue;
+    } else if (no_ops) {
+      cout << 0 << endl;
+      continue;
+    }
+
+    map<long long, vector<long long>> visited;
+    for (long long element: elements) {
+      visited[element % x].push_back(element);
+    }
+
+    long long global_min = n;
+    for (long long i = 0; i < x; ++i) {
+      long long bucket_min = LONG_LONG_MAX;
+      long long curr_val = i;
+      if (visited[i].size() == 0) {
+	global_min = min(global_min, i);
+	continue;
+      }
+      
+      sort(visited[i].begin(), visited[i].end());
+      
+      for (long long j = 0; j < (long long)visited[i].size(); ++j) {
+	if (visited[i][j] > curr_val) {
+	  bucket_min = curr_val;
+	  break;
+	}	
+	curr_val += x;
+	bucket_min = curr_val;
+      }
+      
+      global_min = min(bucket_min, global_min);
+    }
+    
+    cout << global_min << endl;
+  }
+}
