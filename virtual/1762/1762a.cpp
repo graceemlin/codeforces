1c41b959d098a7eb3528da06475cf9665f50e907
graceemlin <110770777+graceemlin@users.noreply.github.com>
2024-10-11 13:49:20 -0700

    cf

---
+++ virtual/1762/1762a.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(int argc, char* argv[]) {
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  int t;
+  cin >> t;
+
+  while (t--) {
+    long long n;
+    cin >> n;
+
+    vector<long long> elements(n);    
+    for (long long i = 0; i < n; ++i) {
+      cin >> elements[i];
+    }
+    
+    long long sum = 0;
+    for (long long element: elements) {
+      sum += element;
+    }
+
+    if (sum % 2 == 0) {
+      cout << 0 << endl;
+      continue;
+    }
+
+    queue<bool> parity;
+    for (long long element: elements) {
+      parity.push(element % 2);
+    }
+    
+    long long ops = 0;
+    bool parity_change_detected = false;
+    
+    while (!parity_change_detected) {
+      ++ops;
+      for (long long i = 0; i < n; ++i) {
+	elements[i] = floor(elements[i] / 2);
+	
+	bool curr_parity = elements[i] % 2;	
+	bool original_parity = parity.front();
+	
+	if (curr_parity != original_parity) {
+	  parity_change_detected = true;
+	  break;
+	}
+
+	parity.pop();
+	parity.push(original_parity);
+      }
+    }
+
+    cout << ops << endl;
+  }
+}
---
+++ virtual/1762/1762b.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(int argc, char* argv[]) {
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  int t;
+  cin >> t;
+
+  while (t--) {
+    long long n;
+    cin >> n;
+    
+    vector<long long> elements(n);
+    for (int i = 0; i < n; ++i) {
+      cin >> elements[i];
+    }
+    
+    unordered_map<long long, list<long long>> value_to_index;
+    
+    for (int i = 0; i < n; ++i) {
+      value_to_index[elements[i]].push_back(i);
+    }
+
+    sort(elements.begin(), elements.end());
+    
+    cout << n << endl;
+    
+    for (int i = 0; i < n; ++i) {
+      list<long long>& indices = value_to_index.find(elements[i])->second;
+      long long index = *indices.begin();
+      indices.erase(indices.begin());
+      
+      long long target = 1;
+      while (elements[i] > target) {
+	target *= 2;
+      }
+      
+      long long x = target - elements[i];
+      
+      cout << index + 1 << ' ' << x << endl;
+    }
+  }
+}
