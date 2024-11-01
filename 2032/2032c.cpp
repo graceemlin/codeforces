eeed754314062f8e4f1e78f5dd1dcbdfeb655586
grace <110770777+graceemlin@users.noreply.github.com>
2024-11-01 12:24:05 -0700

    Update 2032c.cpp

--- 2032/2032c.cpp
+++ 2032/2032c.cpp
@@ -20,7 +20,6 @@ int main(int argc, char* argv[]) {
     sort(elements.begin(), elements.end());
 
     int64_t min_slice = INT64_MAX;
-    
     for (int i = 0; i < n - 1; ++i) {
       auto non_degenerate_bound = lower_bound(elements.begin() + i, elements.end(), elements[i] + elements[i + 1]);
       int64_t current_slice = distance(non_degenerate_bound, elements.end());
