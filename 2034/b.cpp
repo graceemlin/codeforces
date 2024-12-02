ec859c9f34f29f6a1415bedb3373d3b3dd68092a
grace <110770777+graceemlin@users.noreply.github.com>
2024-12-01 21:49:47 -0800

    Create b.cpp

---
+++ 2034/b.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(int argc, char* argv[]) {
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+
+  int t;
+  cin >> t;
+
+  while (t--) {
+    int64_t n;
+    cin >> n;
+    int64_t m;
+    cin >> m;
+    int64_t k;
+    cin >> k;
+
+    string s;
+    cin >> s;
+    
+    int64_t result{};
+    int64_t zero_temp{};
+
+    for (int i = 0; i < s.size(); ++i) {
+      if (s[i] == '0') {
+        ++zero_temp;
+      } else {
+        zero_temp = 0;
+      }
+
+      if (zero_temp == m) {
+        ++result;
+        i += k - 1;
+        zero_temp = 0;
+      }
+    }
+
+    cout << result << endl;
+  }
+}
