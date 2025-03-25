ba6d3a7c33feb369123634abf49e66db184a9ea3
grace <110770777+graceemlin@users.noreply.github.com>
2025-03-25 13:56:58 -0700

    Create 2085a.cpp

---
+++ 2085/2085a.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(int argc, char* argv[]) {
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  int t;
+  cin >> t;
+  
+  while (t--) {
+    int n;
+    cin >> n;
+    int k;
+    cin >> k;
+    string r;
+    cin >> r;
+
+    if (n == 0) {
+      cout << "NO" << endl;
+      continue;
+    }
+    
+    string reversed_string = r;
+    reverse(reversed_string.begin(), reversed_string.end());
+
+    bool possible = true;
+    bool found = false;
+    for (int i = 0; i < r.size(); ++i) {
+      possible &= ((r[i] - 'a') <= (reversed_string[i] - 'a'));
+      if ((r[i] - 'a') < (reversed_string[i] - 'a')) {
+	found = true;
+	break;
+      }
+    }
+    
+    possible &= found;
+    
+    if (k == 0) {
+      cout << (possible ? "YES" : "NO") << endl;
+      continue;
+    }
+
+    set<char> st(r.begin(), r.end());
+    possible |= st.size() > 1;
+    
+    cout << (possible ? "YES" : "NO") << endl;
+  }
+}
